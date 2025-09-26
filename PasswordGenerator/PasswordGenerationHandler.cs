f63dada929adac3970643d68d271023a89a8a921
Ihor Kublytskyi <ihorkublytskyi@gmail.com>
2025-09-26 17:56:09 +0200

    Add project files.

---
+++ PasswordGenerator/PasswordGenerationHandler.cs
@@ -0,0 +1,131 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using TextCopy;
+
+namespace PasswordGenerator;
+
+internal static class PasswordGenerationHandler
+{
+    private static string _lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static string _uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static string _digits = "1234567890";
+    private static string _specialSymbols = "!@#$%^*_";
+
+    internal static void Handle(int length, bool containsUppercaseLetters, bool containsDigits, bool containsSpecialSymbols, int count, bool isCopying)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            string password = Generate(length, containsUppercaseLetters, containsDigits, containsSpecialSymbols, isCopying);
+
+            Console.WriteLine(password);
+
+            if (i == count - 1)
+            {
+                ClipboardService.SetText(password);
+            }
+        }
+    }
+
+    internal static string Generate(int length, bool containsUppercaseLetters, bool containsDigits, bool containsSpecialSymbols, bool isCopying)
+    {
+        string characters = _lowercaseAlphabet;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (containsUppercaseLetters)
+        {
+            characters += _uppercaseAlphabet;
+            stringBuilder.Append(_uppercaseAlphabet[RandomNumberGenerator.GetInt32(_uppercaseAlphabet.Length - 1)]);
+        }
+
+        if (containsDigits)
+        {
+            characters += _digits;
+            stringBuilder.Append(_digits[RandomNumberGenerator.GetInt32(_digits.Length - 1)]);
+        }
+
+        if (containsSpecialSymbols)
+        {
+            characters += _specialSymbols;
+            stringBuilder.Append(_specialSymbols[RandomNumberGenerator.GetInt32(_specialSymbols.Length - 1)]);
+        }
+
+        while (stringBuilder.Length < length)
+        {
+            stringBuilder.Append(characters[RandomNumberGenerator.GetInt32(characters.Length - 1)]);
+        }
+
+        var password = stringBuilder.ToString();
+
+        RandomNumberGenerator.Shuffle(new Span<string>(ref password));
+
+        return password;
+    }
+
+    internal static void CalculateEntropy(string password)
+    {
+        string characters = _lowercaseAlphabet;
+
+        foreach (var character in password)
+        {
+            if (char.IsDigit(character))
+            {
+                characters += _digits;
+            }
+
+            if (char.IsUpper(character))
+            {
+                characters += _uppercaseAlphabet;
+            }
+
+            if (char.IsSymbol(character))
+            {
+                characters += _specialSymbols;
+            }
+        }
+
+        double entropy = (double)password.Length * Math.Log2(characters.Length);
+
+        if (entropy < 40)
+        { 
+            ShowEntropy(ConsoleColor.Red, entropy);
+        }
+        else if (entropy > 40 && entropy < 60)
+        {
+            ShowEntropy(ConsoleColor.Yellow, entropy);
+        }
+        else if (entropy > 60 && entropy < 80)
+        {
+            ShowEntropy(ConsoleColor.DarkGreen, entropy);
+        }
+        else
+        {
+            ShowEntropy(ConsoleColor.Green, entropy);
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    
+    internal static void ShowEntropy(ConsoleColor foregroundColor, double entropy)
+    {
+        Console.Write("Approximate password entropy: ");
+        Console.ForegroundColor = foregroundColor;
+        Console.Write(Math.Round(entropy, 2));
+    }
+
+    internal static void WriteToFile(FileInfo file, string password, string label)
+    {
+        string text = $"{label} = {password}";
+
+        try
+        {
+            File.WriteAllText(file.FullName, text);
+
+            Console.WriteLine("Password has been succesfully written to the file.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Syntax error in file name, folder name, or volume label.");
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.CommandLine;
+
+namespace PasswordGenerator;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        RootCommand rootCommand = new RootCommand("An application for generating strong passwords.");
+
+        Command passwordCommand = new Command("password", "Work with password generation, entropy verification, writing to a file etc.");
+
+        Option<int> lengthOption = new("--length", "-l")
+        {
+            Description = "Sets the password length. Maximum value is 25.",
+            DefaultValueFactory = result => 20
+        };
+
+        lengthOption.Validators.Add(result =>
+        {
+            if(result.GetValue(lengthOption) > 25)
+            {
+                result.AddError("Length must be less than or equal to 25.");
+            }
+            if(result.GetValue(lengthOption) < 4)
+            {
+                result.AddError("Length must be greater than or equal to 4");
+            }
+        });
+
+        Option<bool> uppercaseOption = new("--uppercase", "-u")
+        {
+            Description = "Add capital letters to the password."
+        };
+
+        Option<bool> digitsOption = new("--digit", "-d")
+        {
+            Description = "Add digits too the password."
+        };
+
+        Option<bool> specialSymbolsOption = new("--special", "-s")
+        {
+            Description = "Add special characters to the password."
+        };
+
+        Option<int> countOption = new("--count", "-c")
+        {
+            Description = "Password count.",
+            DefaultValueFactory = parseResult => 1
+        };
+
+        Option<bool> copyOption = new("--copy", "-cp")
+        {
+            Description = "The password will be copied to the clipboard."
+        };
+
+        Command generateCommand = new("generate", "Generate a password.") 
+        {
+            lengthOption,
+            uppercaseOption,
+            digitsOption,
+            specialSymbolsOption,
+            countOption,
+            copyOption
+        };
+
+        generateCommand.SetAction(parseResult => PasswordGenerationHandler.Handle(
+            parseResult.GetValue(lengthOption),
+            parseResult.GetValue(uppercaseOption),
+            parseResult.GetValue(digitsOption),
+            parseResult.GetValue(specialSymbolsOption),
+            parseResult.GetValue(countOption),
+            parseResult.GetValue(copyOption)));
+
+        passwordCommand.Subcommands.Add(generateCommand);
+
+        Argument<string> passwordOption = new("password")
+        {
+            Description = "Password."
+        };
+
+        Command calculateEntropyCommand = new("entropy", "Calculate password`s entropy.")
+        {
+            passwordOption
+        };
+
+        calculateEntropyCommand.SetAction(parseResult => PasswordGenerationHandler.CalculateEntropy(parseResult.GetValue(passwordOption)));
+
+        passwordCommand.Subcommands.Add(calculateEntropyCommand);
+
+        Option<FileInfo> fileArgument = new("--file", "-f")
+        {
+            Description = "Path to file to write to.",
+            DefaultValueFactory = parseResult => 
+            {
+                return new FileInfo($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\ps.txt");
+            }
+        };
+
+        Argument<string> passwordArgument = new("password");
+
+        Argument<string> labelArgument = new("label"); 
+
+        Command writeCommand = new("write", "Write password to a file.") 
+        { 
+            fileArgument, 
+            passwordArgument, 
+            labelArgument 
+        };
+
+        writeCommand.SetAction(parseResult => PasswordGenerationHandler.WriteToFile(
+            parseResult.GetValue(fileArgument),
+            parseResult.GetValue(passwordArgument),
+            parseResult.GetValue(labelArgument)
+            ));
+
+        passwordCommand.Subcommands.Add(writeCommand);
+
+        rootCommand.Subcommands.Add(passwordCommand);
+
+        ParseResult parseResult = rootCommand.Parse(args);
+
+        if(parseResult.Errors.Count == 0)
+        {
+            parseResult.Invoke();
+        }
+
+        foreach (var error in parseResult.Errors)
+        {
+            Console.WriteLine(error.Message);
+        }
+
+        return 1;
+    }
+}
